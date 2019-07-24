c4c74ca279e97391f17e26ee038b28fdd569d61d
Daniel Silva <danielsilva@lululemon.com>
2019-07-24 15:46:29 -0500

    send candidate form

--- src/components/SendCandidateForm/index.js
+++ src/components/SendCandidateForm/index.js
@@ -2,15 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
+    align-items: center;
+    background-image: url(bg.png);
+    background-size: cover;
+    box-sizing: border-box;
+    display: flex;
+    font-size: 30px;
     height:82px;
-    border: 1px red dashed;
+    justify-content: space-between;
     max-width: 1024px;
+    padding: 12px 16px;
+`;
+
+const SendButton = styled.button`
+    width: 210px;
+    height: 54px;
+    background-color: transparent;
+    border: 4px solid #000;
+    font-size: 22px;
 `;
 
 const SendCandidateForm = () => (
     <FormContainer className="form-container">
         <span>Is there anyone else you would want us to add?</span>
-        <button>Submit Name</button>
+        <SendButton>Submit Name</SendButton>
     </FormContainer>
 );
 
